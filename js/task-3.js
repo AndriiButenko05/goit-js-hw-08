8a0fcbb7caee4032f78e42441ea307f0cb7a40ed
Andrii <161694214+AndriiButenko05@users.noreply.github.com>
2025-05-18 15:18:30 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+'use strict';
+function getElementWidth(content, padding, border) {
+    content = parseFloat(content);
+    padding = parseFloat(padding);
+    border = parseFloat(border);
+    const width = content + (padding * 2) + (border * 2);
+    return width;
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px")); 
+console.log(getElementWidth("13px", "10px", "5px"));
